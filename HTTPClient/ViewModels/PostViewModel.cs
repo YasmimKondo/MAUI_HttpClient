1450d821cee5ba8842c9a9e695cd15aee93fd726
Yasmim Kondo <yasmim.kgtx@gmail.com>
2024-08-28 18:26:25 -0300

    feat: initial commit

---
+++ HTTPClient/ViewModels/PostViewModel.cs
@@ -0,0 +1,31 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using HTTPClient.Models;
+using HTTPClient.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace HTTPClient.ViewModels
+{
+    public partial class PostViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        List<Post> posts;
+
+        private ICommand getPostsCommand { get; }
+
+        public PostViewModel()
+        {
+            getPostsCommand = new Command(getPosts);
+        }  
+
+        public async void getPosts()
+        {
+            PostService postService = new PostService();
+            posts = await postService.getPosts();
+        }
+    }
+}
